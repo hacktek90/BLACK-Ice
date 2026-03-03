13317355e45d394c87068d8f0e4ea87d5b787e31
Axrader <79328354+hacktek90@users.noreply.github.com>
2026-03-03 18:26:21 +0530

    Update tracking.js

--- test/tracking.js
+++ test/tracking.js
@@ -8,7 +8,8 @@
     window.__sentryScriptLoaded = true;
 
     const sentry = document.createElement("script");
-    sentry.src = "https://js-de.sentry-cdn.com/d36d10deb3819c24ccbf1a6f76d7042c.min.js";
+    sentry.src =
+      "https://js-de.sentry-cdn.com/d36d10deb3819c24ccbf1a6f76d7042c.min.js";
     sentry.crossOrigin = "anonymous";
     sentry.async = true;
     sentry.id = "sentry-cdn-loader";
@@ -33,11 +34,13 @@
   /* -------------------- GTM NOSCRIPT FALLBACK -------------------- */
   function injectNoScript() {
     if (document.getElementById("gtm-noscript")) return;
+
     var ns = document.createElement("noscript");
     ns.id = "gtm-noscript";
     ns.innerHTML =
       '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W9F9C7ZS" ' +
       'height="0" width="0" style="display:none;visibility:hidden"></iframe>';
+
     document.body.appendChild(ns);
   }
 
@@ -52,15 +55,35 @@
     gtagScript.async = true;
     gtagScript.src =
       "https://www.googletagmanager.com/gtag/js?id=G-BJ7N8R2CLY";
+
     document.head.appendChild(gtagScript);
 
     window.dataLayer = window.dataLayer || [];
+
     function gtag() {
       dataLayer.push(arguments);
     }
+
     window.gtag = gtag;
 
     gtag("js", new Date());
     gtag("config", "G-BJ7N8R2CLY");
+  }
+
+  /* -------------------- CLOUDFLARE PAGES ANALYTICS -------------------- */
+  if (!window.__cfAnalyticsLoaded) {
+    window.__cfAnalyticsLoaded = true;
+
+    var cfScript = document.createElement("script");
+    cfScript.defer = true;
+    cfScript.src =
+      "https://static.cloudflareinsights.com/beacon.min.js";
+
+    cfScript.setAttribute(
+      "data-cf-beacon",
+      '{"token": "b5a71e89ec674296bffb5791b1953eb9"}'
+    );
+
+    document.head.appendChild(cfScript);
   }
 })();
