a25171d4dee903086fd6e53bb81c01a5aa5750d1
Axrader <79328354+hacktek90@users.noreply.github.com>
2026-03-05 22:49:11 +0530

    Update portal.js

--- test/portal.js
+++ test/portal.js
@@ -800,7 +800,7 @@
     })();
 (function () {
   const s = document.createElement("script");
-  s.src = "https://blackice-ac.vercel.app/test/tracking.js";
+  s.src = "/test/tracking.js";
   s.defer = true;
   document.head.appendChild(s);
 })();  
